db8ba8d464a6ad2ab92f60099622f792dc6aec10
Manzana <perezcouch33@gmail.com>
2025-02-07 13:44:36 -0500

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,17 @@
+// productos.js
+const productos = [
+    // Productos Herbi
+    { name: "Canela y clavo", price: 1200, category: "filtrantes", marca: "herbi", image: "http://herbi.com.pe/wp-content/uploads/2022/10/CANELA-Y-CLAVO-X100.png" },
+    { name: "Anis caja", price: 800, category: "filtrantes", marca: "herbi", image: "http://herbi.com.pe/wp-content/uploads/2021/08/ANIS-X100.jpg" },
+    { name: "Boldo caja", price: 25, category: "filtrantes", marca: "herbi", image: "http://herbi.com.pe/wp-content/uploads/2022/10/BOLDO-X100.jpg" },
+    { name: "Mate de Coca caja", price: 500, category: "filtrantes", marca: "herbi", image: "http://herbi.com.pe/wp-content/uploads/2022/10/MATE-DE-COCA-X100.jpg" },
+    { name: "Relax Té caja", price: 100, category: "filtrantes", marca: "herbi", image: "http://herbi.com.pe/wp-content/uploads/2022/10/RELAX-X100.jpg" },
+    { name: "Durazno caja", price: 40, category: "filtrantes", marca: "herbi", image: "http://herbi.com.pe/wp-content/uploads/2022/10/DURAZNO-X100.jpg" },
+    { name: "Uña de Neko caja", price: 100, category: "filtrantes", marca: "herbi", image: "http://herbi.com.pe/wp-content/uploads/2022/10/UNA-DE-GATO-X100.jpg" },
+    { name: "Naranja caja", price: 100, category: "filtrantes", marca: "herbi", image: "http://herbi.com.pe/wp-content/uploads/2022/10/TE-NARANJA-X100.jpg" },
+    { name: "YoYo Bolsa", price: 100, category: "chocolates", marca: "herbi", image: "http://herbi.com.pe/wp-content/uploads/2022/10/Mesa-de-trabajo-4-100-600x599.jpg" },
+    { name: "Manzanilla Caja", price: 100, category: "filtrantes", marca: "herbi", image: "http://herbi.com.pe/wp-content/uploads/2022/10/MANZANILLA-X100.jpg" },
+    { name: "Relax Té caja", price: 100, category: "filtrantes", marca: "rapidin", image: "http://example.com/rapidin.jpg" },
+    { name: "Relax Té caja", price: 150, category: "filtrantes", marca: "villa", image: "http://example.com/devilla.jpg" },
+    // Productos Rapidin
+];
